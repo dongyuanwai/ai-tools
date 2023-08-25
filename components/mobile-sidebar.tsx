c163579a46c4c2fc1e3ed5aa9d42fbcd9799ec04
dongzhiyang <yuanwaidong@gmail.com>
2023-08-25 13:30:16 +0800

    fix
    解决Hydration 问题

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,24 +1,30 @@
 "use client";
-import React from 'react'
+import { useState,useEffect } from 'react'
 import { Button } from './ui/button'
 import { Menu } from 'lucide-react'
-import { Sheet, SheetTrigger,SheetContent } from '@/components/ui/sheet';
+import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet';
 import Sidebar from '@/components/sidebar';
 
 function MobileSidebar() {
-  return (
-    <Sheet>
-        <SheetTrigger>
-            <Button variant="ghost" size="icon"
-                className='md:hidden'>
-                <Menu />
-            </Button>
-        </SheetTrigger>
-        <SheetContent side="left" className='p-0'>
-            <Sidebar></Sidebar>
-        </SheetContent>
-    </Sheet>
-  )
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(()=>{
+        setIsMounted(true)
+    },[])
+    if(!isMounted) return null
+
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Button variant="ghost" size="icon"
+                    className='md:hidden'>
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className='p-0'>
+                <Sidebar></Sidebar>
+            </SheetContent>
+        </Sheet>
+    )
 }
 
 export default MobileSidebar
